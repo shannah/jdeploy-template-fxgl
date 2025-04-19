ad243985ff0a0e8d1b17fbc275df873ba5476dba
Steve Hannah <steve@weblite.ca>
2025-04-18 18:20:37 -0700

    initial commit

---
+++ src/main/java/ca/weblite/testFxgl/Launcher.java
@@ -0,0 +1,10 @@
+package ca.weblite.testFxgl;
+
+import com.almasb.fxgl.app.GameApplication;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        GameApplication.launch(Main.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+open module ca.weblite.testFxgl {
+    requires com.almasb.fxgl.all;
+    exports ca.weblite.testFxgl;
+}
